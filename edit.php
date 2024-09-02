43097fa904b941572bbf25aef151417ef6347d08
GuyGabriell <jonahgabriel85@yahoo.com>
2024-09-02 11:53:54 +0100

    edit.php

--- edit.php
+++ edit.php
@@ -25,7 +25,7 @@
     //GET METHOD:the get method show the clients data 
       if ( !isset($_GET["id"]) ) {
         header("location: /index.php");
-      exit;
+        exit;
       }
 
       $id = $_GET["id"];
@@ -34,10 +34,39 @@
       $result = $connection->query($sql);
       $row = $result->fetch_assoc();
 
+      if (!$row) {
+        header("location: /index.php");
+        exit;
+      }
+
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $address = $_POST["address"];
+
   }else {
-    //POST METHOD:the post method update the clients data 
-    
+    //POST METHOD:the post method update the clients data
+   $id = $_GET["id"];
+   $name = $_POST["name"];
+   $email = $_POST["email"];
+   $phone = $_POST["phone"];
+   $address = $_POST["address"];
+
+   do {
+
+     if ( empty($name) || empty($email) || empty($phone) || empty($address) ) {
+        $errorMessage = "All the fields are required";
+        break;
+
+        $sql = "UPDATE clients" . 
+        "SET name = '$name', email = '$email', phone = '$phone', address = '$address', " . 
+        "WHERE id = $id"; 
+
+   } while (true);
+
   }
+  
+}
 
 ?>
 <!DOCTYPE html>
